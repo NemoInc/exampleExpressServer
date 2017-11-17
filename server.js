938ec1069dfd4703f0ca4335421cdff0cc925f24
kevinpmcc <kevinpatrickmccarthy@gmail.com>
2017-11-17 17:16:12 +0000

    added cors and now sending pure json

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 var express = require("express");
+var cors = require("cors");
 var bodyParser = require("body-parser");
 var app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors())
 
 
 app.get("/", function(req, res) {
-    res.send(JSON.stringify("hello github field day"));
+    res.json({"message": "hello github field day"});
   });
 
 var server = app.listen(3000, function () {
